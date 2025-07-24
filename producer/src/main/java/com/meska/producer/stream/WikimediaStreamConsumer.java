f2c06100d3c047c463c11fb2f199c1f734162f35
Yousef Meska <mrbomberboy123@gmail.com>
2025-07-25 01:04:23 +0300

    added prometheus & grafana - enabling kafka nodes monitoring

--- producer/src/main/java/com/meska/producer/stream/WikimediaStreamConsumer.java
+++ producer/src/main/java/com/meska/producer/stream/WikimediaStreamConsumer.java
@@ -25,6 +25,9 @@ public class WikimediaStreamConsumer {
                 .uri("/stream/recentchange")
                 .retrieve()
                 .bodyToFlux(String.class)
-                .subscribe(log::info);
+                .subscribe((message) -> {
+                    log.info("publishing {}", message);
+                    producer.sendMessage(message);
+                });
     }
 }
